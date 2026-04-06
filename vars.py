c15c8472cdfc7b98a466d17f982adef68b1e2874
mkvbui02-sudo <mkvbui02@gmail.com>
2026-04-06 16:23:00 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23274330"))
-API_HASH = environ.get("API_HASH", "970e2e79779707c56d2b453b3a6eea48")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8756548706:AAEEgcTcHt11BvSIX44l2sRXp9Il-XTnDO8")
+API_ID = int(environ.get("API_ID", "23557588"))
+API_HASH = environ.get("API_HASH", "7ed52f38f74fe85ebc2e5e285051c3b6")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8798811190:AAEgHnasgrTsj-vEvBvO76LjNceQhE0tnvY")
